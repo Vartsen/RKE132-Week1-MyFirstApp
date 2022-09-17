f95f92cf633030e184c22e96a81054b0ac546dfd
Liisi V <liisepp@tktk.ee>
2022-09-17 16:51:25 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib nime
+//rakendus tervitab nimepidi
+
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();  
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName}!");
